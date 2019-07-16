a8cf4b9843bd38dd833e204433c0ab495bb22db7
A <A@As-MacBook-Pro.local>
2019-07-16 16:19:18 -0400

    completed image component

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageService} from './shared/image.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-image',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
-  constructor() { }
+  image: any
+
+  constructor(private imageService: ImageService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    return this.imageService.getImageById(+this.route.snapshot.params['id'])
+    .subscribe(data => this.image= data.find(image => image.id == +this.route.snapshot.params['id']))
   }
 
 }
