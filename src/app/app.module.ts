683defa56c5e1978cbec7ee3ee5a7ccc69a5ec70
A <A@As-MacBook-Pro.local>
2019-07-16 14:37:23 -0400

    created gallery/image components and added routing for them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { AppRoutingModule } from './app-routing.module';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AlertModule } from 'ngx-bootstrap/alert';
+import { ImageComponent } from './image/image.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ImageComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
     AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+
 
   ],
   providers: [],
