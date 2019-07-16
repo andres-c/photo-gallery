01757f7193f24e67d3f2c1cd37826eb13e4640d4
A <A@As-MacBook-Pro.local>
2019-07-16 15:18:21 -0400

    added infinite scroll and completed gallery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AlertModule } from 'ngx-bootstrap/alert';
 import { ImageComponent } from './image/image.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ImageService } from './image/shared/image.service';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
 
 
 
@@ -19,13 +23,13 @@ import { GalleryComponent } from './gallery/gallery.component';
     GalleryComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AlertModule.forRoot(),
     RouterModule.forRoot(appRoutes),
-
+    InfiniteScrollModule
 
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Image } from 'src/app/image.model';
+import {ImageService} from '../image/shared/image.service';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  
+    title="Gallery"
+    images: Image[] = [];
+    page: number = 1;     
 
-  constructor() { }
+  constructor(private imageService: ImageService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {  
+    //call api for initial image gallery rendering. gets first three pages  
+    while (this.page <= 3){
+      this.getImages(); 
+      this.page = this.page + 1;   
+    }
+  }  
 
+  //gets image data from api  
+  getImages() {  
+    console.log(this.page);  
+    this.imageService.getImages(this.page).subscribe((res) => this.onSuccess(res));  
+  }  
+  
+  // when we get data on a success  
+  onSuccess(res) {  
+    console.log(res);  
+    if (res != undefined) {  
+      res.forEach(item => {  
+        this.images.push(item);  
+      });  
+    }  
+  }  
+  
+  //when we scroll down this functions gets called 
+  onScroll()  
+  {  
+    console.log("Scrolled");  
+    this.page = this.page + 1;  
+    this.getImages();  
+  }  
 }
